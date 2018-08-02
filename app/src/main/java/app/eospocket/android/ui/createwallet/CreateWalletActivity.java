b72614fbe0b3098935899dedf3cb3dc2f73ffd80
kai.1001 <kai.1001@kakaocorp.com>
2018-08-02 09:57:33 +0900

    create wallet

--- app/src/main/java/app/eospocket/android/ui/createwallet/CreateWalletActivity.java
+++ app/src/main/java/app/eospocket/android/ui/createwallet/CreateWalletActivity.java
@@ -1,20 +1,47 @@
 package app.eospocket.android.ui.createwallet;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import javax.inject.Inject;
 
 import app.eospocket.android.R;
 import app.eospocket.android.common.CommonActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class CreateWalletActivity extends CommonActivity implements CreateWalletView {
 
     @Inject
     private CreateWalletPresenter mCreateWalletPresenter;
 
+    @BindView(R.id.input_password)
+    EditText mInputPassword;
+
+    @BindView(R.id.input_confirm_password)
+    EditText mInputConfirmPassword;
+
+    @BindView(R.id.agree_lost_password)
+    CheckBox mAgreeLostPasswordChkBox;
+
+    @BindView(R.id.btn_create_wallet)
+    Button mCreateWalletButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createwallet);
+
+        ButterKnife.bind(this);
+
+        mCreateWalletPresenter.onCreate();
+    }
+
+    @OnClick(R.id.btn_create_wallet)
+    public void onCreateWalletClick() {
+
     }
 }
