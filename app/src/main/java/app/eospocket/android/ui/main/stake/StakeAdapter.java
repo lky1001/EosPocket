04bcf6adc15e5ec19781484e49800bf514e7969b
jacob.do <jacob.do@snowcorp.com>
2018-09-07 20:51:28 +0900

    modify stakeViewHolder

--- app/src/main/java/app/eospocket/android/ui/main/stake/StakeAdapter.java
+++ app/src/main/java/app/eospocket/android/ui/main/stake/StakeAdapter.java
@@ -2,32 +2,36 @@ package app.eospocket.android.ui.main.stake;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import app.eospocket.android.R;
 import app.eospocket.android.eos.model.account.EosAccount;
+import app.eospocket.android.ui.base.ViewHolder;
 import app.eospocket.android.ui.main.stake.items.BalanceItem;
 import app.eospocket.android.ui.main.stake.items.ResourceItem;
 import app.eospocket.android.ui.main.stake.items.StakeItem;
 import app.eospocket.android.ui.main.stake.items.StakeUiInfo;
-import app.eospocket.android.ui.main.stake.viewholder.StakeViewHolder;
-import app.eospocket.android.ui.main.stake.viewholder.StakeResourceViewHolder;
-import app.eospocket.android.ui.main.stake.viewholder.StakeBalanceViewHolder;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
-public class StakeAdapter extends RecyclerView.Adapter<StakeViewHolder> {
+public class StakeAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     private List<StakeItem> mItems = new ArrayList<>();
 
     @NonNull
     @Override
-    public StakeViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int layoutResId) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int layoutResId) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(layoutResId, viewGroup, false);
 
         if (layoutResId == R.layout.list_item_balance) {
@@ -40,7 +44,6 @@ public class StakeAdapter extends RecyclerView.Adapter<StakeViewHolder> {
 
     public void refresh(EosAccount eosAccount) {
         mItems.clear();
-
 
         Observable.fromArray(StakeUiInfo.values())
                 .flatMap(stakeModel -> Observable.just(createItem(eosAccount, stakeModel)))
@@ -92,7 +95,7 @@ public class StakeAdapter extends RecyclerView.Adapter<StakeViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(@NonNull StakeViewHolder stakeViewHolder, int i) {
+    public void onBindViewHolder(@NonNull ViewHolder stakeViewHolder, int i) {
         stakeViewHolder.bind(mItems.get(i));
     }
 
@@ -109,5 +112,76 @@ public class StakeAdapter extends RecyclerView.Adapter<StakeViewHolder> {
     @Override
     public long getItemId(int position) {
         return mItems.get(position).getId();
+    }
+
+
+    public class StakeResourceViewHolder extends ViewHolder<ResourceItem> {
+        @BindView(R.id.txt_title)
+        TextView txtTitle;
+        @BindView(R.id.txt_desc)
+        TextView txtDesc;
+        @BindView(R.id.progress)
+        View progress;
+        @BindView(R.id.txt_percentage)
+        TextView txtPercentage;
+
+        public StakeResourceViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @Override
+        public void bind(ResourceItem item) {
+            try {
+                txtTitle.setText(item.getStakeUiInfo().getTitle());
+                txtDesc.setText(getDescription(item));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        private Spannable getDescription(ResourceItem item) throws Exception {
+            int strColor = itemView.getContext().getResources().getColor(item.getStakeUiInfo().getColorResId());
+
+            String used = item.getUsed() + " " + item.getStakeUiInfo().getUnit();
+            String max = item.getMax() + " " + item.getStakeUiInfo().getUnit();
+
+            SpannableString usedStr = new SpannableString(used + " / " + max);
+            usedStr.setSpan(
+                    new ForegroundColorSpan(strColor),
+                    0,
+                    used.length(),
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+
+            return usedStr;
+        }
+    }
+
+    public class StakeBalanceViewHolder extends ViewHolder<BalanceItem> {
+
+        @BindView(R.id.txt_stake_balance_eos)
+        TextView txtStakeBalanceEos;
+        @BindView(R.id.btn_stake)
+        TextView btnStake;
+
+        @BindView(R.id.txt_unstake_eos)
+        TextView txtUnStakeEos;
+        @BindView(R.id.btn_unstake)
+        TextView btnUnStake;
+
+        public StakeBalanceViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @Override
+        public void bind(BalanceItem item) {
+            EosAccount eosAccount = item.getEosAccount();
+
+            long totalEos = eosAccount.cpuWeight + eosAccount.netWeight;
+            txtStakeBalanceEos.setText(String.valueOf(totalEos));
+            txtUnStakeEos.setText(eosAccount.coreLiquidBalance);
+        }
     }
 }
