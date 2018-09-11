0b997331a6866bf6d890b8e163cf5abc007bcbf1
jacob.do <jacob.do@snowcorp.com>
2018-09-11 20:01:33 +0900

    modify UI

--- app/src/main/java/app/eospocket/android/ui/main/MainActivity.java
+++ app/src/main/java/app/eospocket/android/ui/main/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
@@ -151,6 +152,15 @@ public class MainActivity extends CommonActivity implements MainView, Navigation
         }
 
         return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawers();
+            return;
+        }
+        super.onBackPressed();
     }
 
     @Override
